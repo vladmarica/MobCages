84498887149271cc6b2154a9a7af981d69716bcd
Quintinity <vlad.marica1@gmail.com>
2016-04-28 21:26:25 -0300

    pt2

--- src/main/java/quintinity/mods/mobcages/client/MobCageItemRenderer.java
+++ src/main/java/quintinity/mods/mobcages/client/MobCageItemRenderer.java
@@ -1,10 +1,13 @@
 package quintinity.mods.mobcages.client;
 
-import org.lwjgl.opengl.GL11;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.client.IItemRenderer;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 
 public class MobCageItemRenderer implements IItemRenderer 
 {
@@ -12,7 +15,7 @@ public class MobCageItemRenderer implements IItemRenderer
 	
 	public boolean handleRenderType(ItemStack item, ItemRenderType type) 
 	{
-		return type != ItemRenderType.FIRST_PERSON_MAP;
+		return type != ItemRenderType.FIRST_PERSON_MAP; //handle all render types except for maps
 	}
 
 	public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item, ItemRendererHelper helper) 
@@ -22,18 +25,27 @@ public class MobCageItemRenderer implements IItemRenderer
 
 	public void renderItem(ItemRenderType type, ItemStack item, Object... data) 
 	{
-		if (type == ItemRenderType.INVENTORY) {
+		if (type == ItemRenderType.INVENTORY) { //render in inventory
 			GL11.glTranslatef(0F, -0.1F, 0F);
+			GL11.glEnable(GL11.GL_LIGHTING);
 			cage.renderFrame(0, 0, 0, 0, true);
+			GL11.glDisable(GL11.GL_LIGHTING);
+			GL11.glTranslatef(0F, 0.1F, 0F);
 		}
-		else if (type == ItemRenderType.ENTITY) {
-			
+		else if (type == ItemRenderType.ENTITY) { //render in the world as a dropped item
 			GL11.glScalef(0.5F, 0.5F, 0.5F);
 			GL11.glTranslatef(-0.5F, -0.4F, -0.5F);
 			cage.renderFrame(0, 0, 0, 0, false);
 		}
-		else {
+		else { //render in a player's hand
+			GL11.glTranslatef(1F, 0.2F, 1F);
+			GL11.glRotatef(180F, 0F, 1F, 0F);
 			cage.renderFrame(0, 0, 0, 0, false);
+			if (item.hasTagCompound()) {
+				NBTTagCompound tag = item.getTagCompound();
+				EntityLiving entity = cage.getEntity(tag.getString("EntityString"));
+				cage.renderEntity(entity, 0, 0, 0);
+			}
 		}
 	}
 }
